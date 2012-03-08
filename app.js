512dcd57d852b2716475a6db0320649948639d8f
Mattias Ottosson <himself@mattiasottosson.se>
2012-03-08 15:30:05 +0100

    Uncommented app listen to remove LearnBoost/up-errors

--- app.js
+++ app.js
@@ -33,5 +33,6 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+// Uncomment this if you're not using learboost/up
+// app.listen(3000);
+// console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
